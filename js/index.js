386c8c2337bdd563a41f745aac6f0e9f57323839
AnnA <50335792+luqu33n@users.noreply.github.com>
2019-08-13 22:24:38 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let url = "./partials/home.html";
+const container = document.querySelector(".container");
+const errorContainer = document.querySelector(".error");
+const links = document.querySelectorAll("nav a");
+
+function loadContent(urlValue) {
+
+	 fetch(urlValue)
+    
+        .then(function (response) {
+          if (response.statusText === "OK") {
+              return response.text();
+          }
+              throw new Error(response.statusText)
+          })
+    
+       .then(function (data) {
+           container.innerHTML = data;
+       })
+    
+       .catch(function (err) {
+          errorContainer.textContent = `${err.name}: ${err.message}`;
+       });
+  } 
+
+function handleClick(ev) {
+
+	ev.preventDefault();
+
+	let currentLink = ev.target;
+
+	let url = currentLink.href;
+
+	loadContent(url);
+}
+
+for (let link of links) {
+	link.addEventListener("click", handleClick);
+}
